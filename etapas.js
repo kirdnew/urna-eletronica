e81f7630e4fbb22e1e9c54f5a06347b33fa8f714
Wendrik Ramos <wendrik.r@protonmail.com>
2021-09-14 17:49:18 -0300

    script de etapas

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Frodo',
+                partido: 'PHM',
+                fotos: [
+                    {url: '38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Sam',
+                partido: 'HTC',
+                fotos: [
+                    {url: '77222.jpg', legenda: 'Vereador'}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Gandalf',
+                partido: 'MTM',
+                vice: 'Saruman',
+                fotos: [
+                    {url:'99.jpg', legenda: 'Prefeito'},
+                    {url:'99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 99,
+                nome: 'Elrond',
+                partido: 'ETM',
+                vice: 'Galandriel',
+                fotos: [
+                    {url:'84.jpg', legenda: 'Prefeito'},
+                    {url:'84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+]
