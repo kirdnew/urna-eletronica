ee0dde9e515f978f5d662ca40d8ccfafaed718ed
Wendrik Ramos <wendrik.r@protonmail.com>
2021-09-16 18:53:44 -0300

    funcionalidades completas

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # urna-eletronica
 Urna eletrônica de votação feita em HTML, CSS e Javascript
+
+Candidatos disponíveis:
+    VEREADOR: 38111 ou 77222
+    PREFEITO: 99 ou 84
--- etapas.js
+++ etapas.js
@@ -4,7 +4,7 @@ let etapas = [
         numeros: 5,
         candidatos: [
             {
-                numero: 38111,
+                numero: '38111',
                 nome: 'Frodo',
                 partido: 'PHM',
                 fotos: [
@@ -12,7 +12,7 @@ let etapas = [
                 ]
             },
             {
-                numero: 77222,
+                numero: '77222',
                 nome: 'Sam',
                 partido: 'HTC',
                 fotos: [
@@ -26,7 +26,7 @@ let etapas = [
         numeros: 2,
         candidatos: [
             {
-                numero: 99,
+                numero: '99',
                 nome: 'Gandalf',
                 partido: 'MTM',
                 vice: 'Saruman',
@@ -36,7 +36,7 @@ let etapas = [
                 ]
             },
             {
-                numero: 99,
+                numero: '84',
                 nome: 'Elrond',
                 partido: 'ETM',
                 vice: 'Galandriel',
